4a51cf90ec92b08f91748e56595a2f5a439748b0
Alicja M <166336325+All0Mi@users.noreply.github.com>
2024-04-07 23:26:49 +0200

    Add files via upload
    Ready project from Uni repo

---
+++ src/include.sv
@@ -0,0 +1,11 @@
+`ifndef ALU_DEF
+
+`define ALU_DEF
+`include "./bit_set/bit_set.sv"
+`include "./bit_shift/bit_shift.sv"
+`include "./comparator/comparator.sv"
+`include "./u2_to_sm/u2_to_sm.sv"
+`include "./MUX.sv"
+`include "./reg.sv"
+  
+`endif
